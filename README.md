5e1b4114738a7eb6258819c966ce02f66093541c
Sophy Lee <stlee26@gmail.com>
2014-04-21 20:58:32 -0700

    basic features

---
+++ README.md
@@ -0,0 +1,3 @@
+# Calculator
+
+Open `index.html`
---
+++ js/script.js
@@ -0,0 +1,86 @@
+$(function (){
+  this.total = 0;
+  this.queue;
+
+  $("#one").click(function () {
+    $(".screen").append("1");
+  });
+
+  $("#two").click(function () {
+    $(".screen").append("2");
+  });
+
+  $("#three").click(function () {
+    $(".screen").append("3");
+  });
+
+  $("#four").click(function () {
+    $(".screen").append("4");
+  });
+
+  $("#five").click(function () {
+    $(".screen").append("5");
+  });
+
+  $("#six").click(function () {
+    $(".screen").append("6");
+  });
+
+  $("#seven").click(function () {
+    $(".screen").append("7");
+  });
+
+  $("#eight").click(function () {
+    $(".screen").append("8");
+  });
+
+  $("#nine").click(function () {
+    $(".screen").append("9");
+  });
+
+  $("#zero").click(function () {
+    $(".screen").append("0");
+  });
+
+  $("#plus").click(function () {
+    window.num = $(".screen").text();
+    window.sign = "+";
+    $(".screen").empty();
+  });
+
+  $("#minus").click(function () {
+    window.num = $(".screen").text();
+    window.sign = "-";
+    $(".screen").empty();
+  });
+
+  $("#times").click(function () {
+    window.num = $(".screen").text();
+    window.sign = "*";
+    $(".screen").empty();
+  });
+
+  $("#divide").click(function () {
+    window.num = $(".screen").text();
+    window.sign = "/";
+    $(".screen").empty();
+  });
+
+  $("#equals").click(function () {
+      window.num_two = $(".screen").text();
+      $(".screen").empty();
+      if (window.sign === "+") {
+        window.num = parseInt(window.num) + parseInt(window.num_two);
+        $(".screen").append(window.num);
+      } else if (window.sign === "-") {
+        window.num = window.num - window.num_two;
+        $(".screen").append(window.num);
+      } else if (window.sign === "*") {
+        window.num = window.num * window.num_two;
+        $(".screen").append(window.num);
+      } else if (window.sign === "/") {
+        window.num = window.num / window.num_two;
+        $(".screen").append(window.num);
+      };
+  });
+})
